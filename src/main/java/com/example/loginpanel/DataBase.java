b9afd972dbf17e8c336e401c14cd5aa52f4702c5
Ko1da3vN1k1ta <666.shadow.999@mail.ru>
2023-10-30 17:48:05 +0300

    fdsfs

--- src/main/java/com/example/loginpanel/DataBase.java
+++ src/main/java/com/example/loginpanel/DataBase.java
@@ -5,46 +5,47 @@ import java.util.List;
 
 public class DataBase {
     public static List<User> users = new ArrayList<>();
+
     static {
-        users.add(new User("admin","admin","Vitaliy","Koshkin"));
-        users.add(new User("2511","koldaev2005","Nikita","koldaev"));
+        users.add(new User("1234","admin","Vitaliy","Kos"));
+        users.add(new User("2511","k","Nikita","ko"));
     }
 
     public static List<String> items = new ArrayList<>();
     public static int indexUser = 0;
-    public static boolean isValidUser(String login) {
 
-        for(int i = 0; i < users.size(); i++) {
-            System.out.println("Check login to" + users.get(i).getLogin());
-            if(!users.get(i).getLogin().equals(login)) {
+    public static boolean isValidUser(int r, String login) {
+        System.out.println("Check login to" + users.get(r).getLogin());
+        if (!users.get(r).getLogin().equals(login)) {
 
-                System.out.println("No valid");
-                return false;
+            System.out.println("No valid");
+            return false;
 
-            } else {
-                System.out.println("valid");
-                indexUser = i;
-                return true;
-            }
+        } else {
+            System.out.println("valid");
+            indexUser = r;
+            return true;
         }
-        return false;
     }
 
-    public static boolean checkPassword(String login, String password){
-        if(isValidUser(login)) {
-            for (int i = 0; i < users.size(); i++) {
-                if (users.get(i).getLogin().equals(login)) {
-                    if(users.get(i).getPassword().equals(password)) {
-                        return true;
+    public static boolean checkPassword(String login, String password) {
+        boolean result = false;
+        for (int e = 0; e < users.size(); e++) {
+            if (isValidUser(e,login)) {
+                for (int i = 0; i < users.size(); i++) {
+                    if (users.get(i).getLogin().equals(login)) {
+                        if (users.get(i).getPassword().equals(password)) {
+                            result = true;
+                            break;
+                        }
+                    } else {
+                        result = false;
                     }
-                } else {
-                    return false;
                 }
+            } else {
+                result = false;
             }
-        } else {
-            return false;
         }
-        return false;
+        return result;
     }
 }
-
